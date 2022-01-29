6c6ca4e67c56a59570095016b1b9f0fbb64cbe77
TAO <santiagooheernandez@gmail.com>
2022-01-29 13:38:19 -0400

    Function "home" added.

---
+++ array.lua
@@ -0,0 +1,11 @@
+mt = {}
+
+N = 3
+M = 3
+
+for i = 1, N do
+  mt[i] = {}
+  for j = 1, M do
+    mt[i][j] =0
+  end
+end
--- digIt.lua
+++ digIt.lua
@@ -1,61 +1,68 @@
-z = 3
-x = 2
-y = 4
+z = 4
+x = 3
+y = 100
 
 function goBack()
-    turtle.turnRight()
-    turtle.turnRight()
+  turtle.turnRight()
+  turtle.turnRight()
+  for o = -1, z do
     turtle.forward()
-    turtle.forward()
-    turtle.forward()
-    turtle.turnLeft() 
+  end
+  turtle.turnLeft()
 end
 
-print("¡Tarea Iniciada!")
-for l = 1, y, 1 do
-    turtle.digDown()
-    turtle.down()
-    for i = 1, x, 1 do
-        for j = 1, z, 1 do
-            if j > 3 then
-                turtle.turnLeft()
-                turtle.turnLeft()
-                turtle.select(2)
-                turtle.placeUp()
-                turtle.turnLeft()
-                turtle.turnLeft()
-                turtle.dig()
-                turtle.forward()
-            else
-                turtle.dig()
-                turtle.forward()
-            end
-        end
-        turtle.turnLeft()
-        turtle.dig()
-        turtle.forward()
-        turtle.turnLeft()
-        for k = 1, z, 1 do
-            turtle.dig()
-            turtle.forward()
-        end
-        turtle.turnRight()
-        turtle.dig()
-        turtle.forward()
-        turtle.turnRight()
-    end
-    turtle.turnLeft()
-    turtle.back()
-    turtle.select(1)
-    turtle.place()
-    goBack()
-end
-for m = 1, y, 1 do
+
+function home()
+  for m = 1, y, 1 do
     turtle.forward()
     turtle.up()
     turtle.turnLeft()
     turtle.turnLeft()
+    for n = 1, 2, 1 do
+      turtle.select(2)
+      turtle.place()
+    end
+  end
 end
+
+
+print("¡Tarea Iniciada!")
+
+for l = 1, y, 1 do
+
+      turtle.digDown()
+      turtle.down()
+
+  for i = 1, x, 1 do
+    for j = 1, z, 1 do
+      if j > 3 then
+        turtle.dig()
+        turtle.forward()
+      else
+        turtle.dig()
+        turtle.forward()
+      end
+    end
+    turtle.turnLeft()
+    turtle.dig()
+    turtle.forward()
+    turtle.turnLeft()
+    for k = 1, z, 1 do
+      turtle.dig()
+      turtle.forward()
+    end
+    turtle.turnRight()
+    turtle.dig()
+    turtle.forward()
+    turtle.turnRight()
+  end
+  turtle.turnLeft()
+  turtle.back()
+  turtle.select(1)
+  turtle.place()
+  goBack()
+end
+home()
 turtle.back()
 turtle.back()
 print("¡Tarea Terminada!")
